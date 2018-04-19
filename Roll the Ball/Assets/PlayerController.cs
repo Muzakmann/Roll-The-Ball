0f6d061c041c13da34230876909cb9fc4907fe4d
Muzakmann <zacharycrane24@gmail.com>
2018-04-19 16:45:34 -0400

    Full Roll the Ball Program
    Full Roll the Ball Program

---
+++ Roll the Ball/Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public Text WinText;
+    public float speed;
+    public Text countText;
+    private int count = 0;
+    Rigidbody rb;
+	
+    
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        WinText.text = "";
+	}
+
+
+    void CountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 12)
+        {
+            WinText.text = "You Win!";
+
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            CountText(); 
+           
+        }
+    }
+  
+}
+
+
